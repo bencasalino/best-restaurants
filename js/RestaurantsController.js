549115d31ceceb985dcdbf02e2350debae0184f8
Ben Casalino & Tim White <student@epicodus.com>
2015-09-21 11:32:03 -0700

    added new object properties to controller

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,8 +1,11 @@
 bestRestaurants.controller('RestaurantsCtrl', function RestaurantsCtrl($scope){
     $scope.restaurants = [];
     $scope.addRestaurant = function(){
-        $scope.restaurants.push({ name: $scope.restaurantName });
+        $scope.restaurants.push({ name: $scope.restaurantName, cuisine: $scope.restaurantCuisine, location: $scope.restaurantLocation, priceRange: $scope.restaurantPriceRange });
         $scope.restaurantName = null;
+        $scope.restaurantCuisine = null;
+        $scope.restaurantLocation = null;
+        $scope.restaurantPriceRange = null;
     };
     $scope.deleteResaurant = function(restaurant){
         var index = $scope.restaurants.indexOf(restaurant);
