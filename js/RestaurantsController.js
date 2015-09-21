b12cdf911a74e8dc4ab58cb2e85761bbd1ab4554
Ben Casalino & Tim White <student@epicodus.com>
2015-09-21 11:22:23 -0700

    add restaurant controller

---
+++ js/RestaurantsController.js
@@ -0,0 +1,11 @@
+bestRestaurants.controller('RestaurantsCtrl', function RestaurantsCtrl($scope){
+    $scope.restaurants = [];
+    $scope.addRestaurant = function(){
+        $scope.restaurants.push({ name: $scope.restaurantName });
+        $scope.restaurantName = null;
+    };
+    $scope.deleteResaurant = function(restaurant){
+        var index = $scope.restaurants.indexOf(restaurant);
+        $scope.restaurants.splice(index, 1);
+    };
+});
