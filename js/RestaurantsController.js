bf7706ce4809c01464d4573d1261762b183f7ae2
Ben Casalino & Tim White <student@epicodus.com>
2015-09-21 13:58:46 -0700

    add text query functionality

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,12 +1,13 @@
 bestRestaurants.controller('RestaurantsCtrl', function RestaurantsCtrl($scope){
     $scope.restaurants = [];
     $scope.addRestaurant = function(){
-        $scope.restaurants.push({ name: $scope.restaurantName, cuisine: $scope.restaurantCuisine, location: $scope.restaurantLocation, priceRange: $scope.restaurantPriceRange });
+        $scope.restaurants.push({ name: $scope.restaurantName, cuisine: $scope.restaurantCuisine, location: $scope.restaurantLocation, minPrice: $scope.restaurantMinPrice, maxPrice: $scope.restaurantMaxPrice });
         $scope.restaurantName = null;
         $scope.restaurantCuisine = null;
         $scope.restaurantLocation = null;
-        $scope.restaurantPriceRange = null;
-    };
+        $scope.restaurantMinPrice = null;
+        $scope.restaurantMaxPrice = null;   
+};
     $scope.deleteResaurant = function(restaurant){
         var index = $scope.restaurants.indexOf(restaurant);
         $scope.restaurants.splice(index, 1);
